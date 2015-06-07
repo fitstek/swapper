80361b7b81fed1e20b62b7783794c1685467f926
Fitsum Teklehaimanot <fitsum.teklehaimanot@gmail.com>
2015-06-07 17:30:30 +0100

    Initial Commit

---
+++ swapper/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  swapper
+//
+//  Created by Fitsum Teklehaimanot on 07/06/2015.
+//  Copyright (c) 2015 Fitsum Teklehaimanot. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  printf("Hello, World!\n");
+    return 0;
+}
